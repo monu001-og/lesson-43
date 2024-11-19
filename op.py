f3584cafa667434c2c5ca5146059f852d8f5ec96
monu001-og <olatudeogbe@gmail.com>
2024-11-19 06:41:03 +0100

    op.py

---
+++ op.py
@@ -0,0 +1,17 @@
+#Open file and its commments
+file=open('Codingal.txt','r')
+print(file.read())
+file.close()
+
+#open file and read its beginning 8 characters
+file=open('Codingal.txt','r')
+print("\n Read  in parts \n")
+print(file.read(8))
+file.close()
+
+#opened your name and age in the file
+file = open('Codingal.txt','a')
+file.write(("Hi I am Penguin and I am 1 yr old"))
+file.close()
+
+
